14674f5f5d422ff94f922c22da068fea36f6cb16
Tristan Pacba <tpacba@live.com>
2020-08-08 22:05:15 -0700

    added parameter, send the user back if they're too far from the coast

--- app-search.js
+++ app-search.js
@@ -22,16 +22,25 @@ $(document.body).ready(function () {
                 }
             })
 
-            // Create div elements for the search results then append it, apply a stock photo if database doesn't have one, create a data-index for later reference
-            coastalinfo.forEach(function (item, index) {
-                if (item.Photo_1 == "") {
-                    var element = $('<div class="row result" id="odd-row" data-index="' + index + '"><div class="col-md-6"><div class="info-div">' + item.NameMobileWeb + '</div></div><div class="col-md-6"><div class="pic-div"><img src="images/beach-sand.jpg" alt="..." class="img-thumbnail"></div></div></div><br>');
-                    $(".searches").append(element);
-                } else {
-                    var element = $('<div class="row result" id="odd-row" data-index="' + index + '"><div class="col-md-6"><div class="info-div">' + item.NameMobileWeb + '</div></div><div class="col-md-6"><div class="pic-div"><img src="' + item.Photo_1 + '" alt="..." class="img-thumbnail"></div></div></div><br>');
-                    $(".searches").append(element);
-                }
-            })
+            if (coastalinfo.length == 0) {
+                // Alert user if no searches match the coastal commisions API, send them back to home page
+                alert("Please try a closer location!")
+                window.location.href = "index.html";
+            } else {
+                // Create div elements for the search results then append it, apply a stock photo if database doesn't have one, create a data-index for later reference
+                coastalinfo.forEach(function (item, index) {
+                    console.log(item);
+                    if (item.Photo_1 == "") {
+                        var element = $('<div class="row result" id="odd-row" data-index="' + index + '"><div class="col-md-6"><div class="info-div"><p>' + item.NameMobileWeb + '</p><p>' + item.COUNTY + ' County, California</p></div></div><div class="col-md-6"><div class="pic-div"><img src="images/beach-sand.jpg" alt="..." class="img-thumbnail"></div></div></div><br>');
+                        $(".searches").append(element);
+                    } else {
+                        var element = $('<div class="row result" id="odd-row" data-index="' + index + '"><div class="col-md-6"><div class="info-div"><p>' + item.NameMobileWeb + '</p><p>' + item.COUNTY + ' County, California</p></div></div><div class="col-md-6"><div class="pic-div"><img src="' + item.Photo_1 + '" alt="..." class="img-thumbnail"></div></div></div><br>');
+                        $(".searches").append(element);
+                    }
+                })
+            }
+
+
 
             // When user clicks on the div row, use the data-index to reference which of the results in the coastalinfo array is being referred to
             $(document).on("click", ".result", function () {
